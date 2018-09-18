9cc66a69faaf81a21a99f5e9b7e405d676b0a282
Owen Hardy <OwenRay@users.noreply.github.com>
2018-09-19 00:26:42 +0200

    added one-line-installer

---
+++ sentinel-one-line-installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~
+
+sudo apt-get update
+sudo apt-get -y install python-virtualenv
+
+export LC_ALL="en_US.UTF-8" && export LC_CTYPE="en_US.UTF-8"
+git clone https://github.com/zocteam/sentinel.git && cd sentinel
+virtualenv ./venv
+./venv/bin/pip install -r requirements.txt
+
+echo "* * * * * cd $(pwd) && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> /etc/crontab
