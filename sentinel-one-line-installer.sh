8cfe739b1b5a2e588bccfc84a38263d693280a2c
Owen Hardy <OwenRay@users.noreply.github.com>
2018-09-19 01:16:23 +0200

    set different sentinel directory to prevent conflicts

--- sentinel-one-line-installer.sh
+++ sentinel-one-line-installer.sh
@@ -7,7 +7,8 @@ sudo apt-get -y install python-virtualenv
 sudo locale-gen "en_US.UTF-8"
 
 export LC_ALL="en_US.UTF-8" && export LC_CTYPE="en_US.UTF-8"
-git clone https://github.com/zocteam/sentinel.git && cd sentinel
+git clone https://github.com/zocteam/sentinel.git zoc_sentinel
+cd zoc_sentinel
 virtualenv ./venv
 ./venv/bin/pip install -r requirements.txt
 
