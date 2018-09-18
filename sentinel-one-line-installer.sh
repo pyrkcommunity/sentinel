c66554885ea732e52afcf1d7bb7358c09d80f766
Owen Hardy <OwenRay@users.noreply.github.com>
2018-09-19 01:11:01 +0200

    fixed locale issue

--- sentinel-one-line-installer.sh
+++ sentinel-one-line-installer.sh
@@ -4,6 +4,7 @@ cd ~
 
 sudo apt-get update
 sudo apt-get -y install python-virtualenv
+sudo locale-gen "en_US.UTF-8"
 
 export LC_ALL="en_US.UTF-8" && export LC_CTYPE="en_US.UTF-8"
 git clone https://github.com/zocteam/sentinel.git && cd sentinel
