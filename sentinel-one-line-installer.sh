dfd0d3aff0e834d59eb6adf7c259d7f3d01a49bf
Stuart Raynor <madstu@gmail.com>
2018-09-19 09:09:52 +0100

    Update sentinel-one-line-installer.sh

--- sentinel-one-line-installer.sh
+++ sentinel-one-line-installer.sh
@@ -4,12 +4,15 @@ cd ~
 
 sudo apt-get update
 sudo apt-get -y install python-virtualenv
-sudo locale-gen "en_US.UTF-8"
+#sudo locale-gen "en_US.UTF-8"
 
-export LC_ALL="en_US.UTF-8" && export LC_CTYPE="en_US.UTF-8"
+#export LC_ALL="en_US.UTF-8" && export LC_CTYPE="en_US.UTF-8"
 git clone https://github.com/zocteam/sentinel.git zoc_sentinel
 cd zoc_sentinel
 virtualenv ./venv
 ./venv/bin/pip install -r requirements.txt
 
-echo "* * * * * cd $(pwd) && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> /etc/crontab
+crontab -l > mycron
+echo "* * * * * cd $(pwd) && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> mycron
+crontab mycron
+rm mycron
